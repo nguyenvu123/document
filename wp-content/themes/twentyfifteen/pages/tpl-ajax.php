6891e76f86135a6642515c0b690f6803860524ca
vunguyen <vunguyen475@gmail.com>
2017-12-17 23:09:55 +0700

    add plugins

---
+++ wp-content/themes/twentyfifteen/pages/tpl-ajax.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: page-ajax
+*/
+?>
+<?php get_header(); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h2>page ajax</h2>
+</body>
+</html>
+<?php get_footer(); ?>
